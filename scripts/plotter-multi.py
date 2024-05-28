0557d141c456331be41fd485a4c1879af81478d1
motangpuar <ninoagus@protonmail.com>
2024-05-28 19:46:42 +0000

    Dirty commit, will clean later

--- scripts/plotter-multi.py
+++ scripts/plotter-multi.py
@@ -1,22 +1,26 @@
 import matplotlib.pyplot as plt
 import glob
+import numpy as np
+import seaborn as sns
 
-# for call in mpi_calls:
-#     print(call)
-#     print(len(mins[call]))
-#     generate_file(mins[call], call, "time")
+def generate_file_reprompi(master_data_array, calls, measure_type, metric, param):
+    #points = range(1,49)
 
-def generate_file(master_data_array, calls, measure_type):
-    metric="time"
-    points = range(1,37)
+    for item in master_data_array:
+        print(item)
+        points = range(1,len(master_data_array[item])+1)
+        print(points)
+
     filename = f"data/dump_{measure_type}.txt"
     with open(filename, "w") as file:
         for call in calls:
             data_array = master_data_array[call]
-            file.write(f"PARAMETER p\n")
+            file.write(f"PARAMETER {param}\n")
             file.write("POINTS " + " ".join(f"({p})" for p in points) + "\n")
             file.write(f"REGION {call}\n")
             file.write(f"METRIC {metric}\n")
+
+            print(len(data_array))
 
             for i, value in enumerate(data_array):
                 #print(i,value)
@@ -24,101 +28,210 @@ def generate_file(master_data_array, calls, measure_type):
 
     print(f"File '{filename}' generated successfully.")
 
+def process_file_sort():
+    # Get the list of files with incremental names
+    file_pattern = 'data/sort_np*.log'
+    files = sorted(glob.glob(file_pattern))
 
-# Get the list of files with incremental names
-file_pattern = 'data/bmark_np*_nrep*.log'
-files = sorted(glob.glob(file_pattern))
+    print(files)
 
-# Parse the data from each file
-data = {}
-means = {}
-mins = {}
-maxs = {}
-for file_path in files:
-    with open(file_path, 'r') as file:
-        document_content = file.read()
+    data = {}
 
-    current_call = None
-    for line in document_content.split('\n'):
-        if line.startswith('#@nprocs'):
-            print(line)
-            index = line.split('=')
-            print(index[1])
-        if not line.startswith('#'):
-            current_call = line.strip()
-            if current_call.startswith('MPI_'):
-                test, _, nrep, valid_nrep, mean_runtime, min_runtime, max_runtime = current_call.split()
-                print(min_runtime)
-                if test not in means:
-                    means[test] = []
-                if test not in mins:
-                    mins[test] = []
-                if test not in maxs:
-                    maxs[test] = []
-                means[test].append(float(mean_runtime))
-                mins[test].append(float(min_runtime))
-                maxs[test].append(float(max_runtime))
-                print(means)
+    data['radix'] = []
+    data['qsort'] = []
+    data['omp'] = []
 
-# Calculate mean, min, and max for each MPI call
-mpi_calls = list(means.keys())
-#print(mpi_calls)
-# means = {}
-# mins = {}
-# maxs = {}
-# for call in mpi_calls:
-#     print(f"{call}: {len(data[call])}")
-#     means[call] = []
-#     mins[call] = []
-#     maxs[call] = []
-#     for i in range(len(files)):
-#         means[call].append(sum(data[call][i * 1000 : (i + 1) * 1000])/len(files))  # Convert to milliseconds
-#         mins[call].append(min(data[call][i * 1000 : (i + 1) * 1000]))  # Convert to milliseconds
-#         maxs[call].append(max(data[call][i * 1000 : (i + 1) * 1000]))  # Convert to milliseconds
-# 
-#     print(f"{call}-mean: {len(means[call])}")
+    means = {}
+    mins = {}
+    maxs = {}
+    for file_path in files:
+        with open(file_path, 'r') as file:
+            document_content = file.read()
+        current_file = [] 
+        current_call = None
+        c_data = {}
+        c_data['radix'] = []
+        c_data['qsort'] = []
+        c_data['omp'] = []
+        for line in document_content.split('\n'):
+            if len(line) > 0:
+                _, _, sort_type, runtime = line.split()
+                print(runtime)
+                
+                sort_type = sort_type[:-1]
+                print(sort_type)
+                c_data[sort_type].append(float(runtime))
 
+        print(c_data)
+        data['radix'].append(np.mean(c_data['radix']))
+        data['qsort'].append(np.mean(c_data['qsort']))
+        data['omp'].append(np.mean(c_data['omp']))
 
-print(mins)
+    metric = "time (s)"
+    keys = list(data.keys())
+    generate_file_reprompi(data, keys, "sort", metric, "Process Num")
 
-generate_file(mins, mpi_calls, "min")
-generate_file(means, mpi_calls, "mean")
-generate_file(maxs, mpi_calls, "max")
+def process_file_cppmafia():
+    # Get the list of files with incremental names
+    file_pattern = 'data/cppmafia_np*.log'
+    files = sorted(glob.glob(file_pattern))
 
+    print(files)
 
-# Plot the data for each MPI call
-x = range(1, len(files) + 1)
+    data = {}
 
-for call in mpi_calls:
-    plt.figure(figsize=(8, 8))
+    data['min'] = []
+    data['mean'] = []
+    data['max'] = []
+    for file_path in files:
+        with open(file_path, 'r') as file:
+            document_content = file.read()
+        current_file = [] 
+        current_call = None
+        for line in document_content.split('\n'):
+            if len(line) > 0:
+                _, _, runtime, metric = line.split()
+                current_file.append(float(runtime))
+                print(current_file)
+                print(len(current_file))
+       
+        #data['MAFIA'].append(sum(current_file)/len(current_file))
+        data['mean'].append(np.mean(current_file))
+        data['min'].append(np.min(current_file))
+        data['max'].append(np.max(current_file))
+
+    metric="time (s)"
+    keys = list(data.keys())
+
+    generate_file_reprompi(data, keys, "cppmafia", metric, "k")
+    plot_cppmafia(data, files, metric)
+
+def process_file_reprompi():
+    # Get the list of files with incremental names
+    file_pattern = 'data/bmark_np*_nrep*.log'
+    files = sorted(glob.glob(file_pattern))
     
-    # Plot means
-    plt.subplot(3, 1, 1)
-    plt.plot(x, means[call], marker='o', label='Mean')
-    plt.xticks(x, rotation=45)
-    plt.xlabel('Process Number')
-    plt.ylabel('Runtime (ms)')
-    plt.title(f'{call} - Mean Runtime')
-    plt.legend()
+    print(files)
     
-    # Plot mins
-    plt.subplot(3, 1, 2)
-    plt.plot(x, mins[call], marker='v', label='Min')
-    plt.xticks(x, rotation=45)
-    plt.xlabel('Process Number')
-    plt.ylabel('Runtime (ms)')
-    plt.title(f'{call} - Min Runtime')
-    plt.legend()
+    # Parse the data from each file
+    data = {}
+    means = {}
+    mins = {}
+    maxs = {}
+    for file_path in files:
+        with open(file_path, 'r') as file:
+            document_content = file.read()
     
-    # Plot maxs
-    plt.subplot(3, 1, 3)
-    plt.plot(x, maxs[call], marker='^', label='Max')
-    plt.xticks(x, rotation=45)
-    plt.xlabel('Process Number')
-    plt.ylabel('Runtime (ms)')
-    plt.title(f'{call} - Max Runtime')
-    plt.legend()
+        current_call = None
+        for line in document_content.split('\n'):
+            if line.startswith('#@nprocs'):
+                print(line)
+                index = line.split('=')
+                print(index[1])
+            if not line.startswith('#'):
+                current_call = line.strip()
+                if current_call.startswith('MPI_'):
+                    test, _, nrep, valid_nrep, mean_runtime, min_runtime, max_runtime = current_call.split()
+                    print(min_runtime)
+                    if test not in means:
+                        means[test] = []
+                    if test not in mins:
+                        mins[test] = []
+                    if test not in maxs:
+                        maxs[test] = []
+                    means[test].append(float(mean_runtime)*1*1000)
+                    mins[test].append(float(min_runtime)*1*1000)
+                    maxs[test].append(float(max_runtime)*1*1000)
+                    print(means)
     
+    mpi_calls = list(means.keys())
+    metric = "time (ms)"
+    generate_file_reprompi(mins, mpi_calls, "reprompi_min", metric, "Process Num")
+    generate_file_reprompi(means, mpi_calls, "reprompi_mean", metric, "Process Num")
+    generate_file_reprompi(maxs, mpi_calls, "reprompi_max", metric, "Process Num")
+
+    plot_reprompi(mins, means, maxs, files)
+
+def plot_cppmafia(data, files, metric):
+
+    index = list(data.keys())
+    
+    # Plot the data for each MPI call
+    x = range(1, len(files)+1)
+    x_ticks = list(range(1, len(files), 8))
+    x_ticks.append(len(files))
+    
+    print(data)
+    plt.figure(figsize=(16, 12))
+    #sns.set_theme(palette="Blues")
+    i=1
+    for call in data:
+    
+        # Plot means
+        plt.subplot(len(data), 1, i)
+        plt.grid(True)
+        plt.plot(x, data[call], marker='o', label='Mean')
+        plt.xticks(x_ticks)
+        plt.xlabel('k')
+        plt.ylabel(metric)
+        plt.title(f'{call} Runtime')
+        plt.legend()
+
+        i=i+1
+
+
     plt.tight_layout()
-    plt.savefig(f'plots/performance_metrics_{call}.png')
+    plt.savefig(f'plots/performance_metrics_MAFIA.png')
     plt.close()
+
+def plot_reprompi(mins, means, maxs, files):
+
+    mpi_calls = list(means.keys())
+    
+    # Plot the data for each MPI call
+    x = range(1, len(files)+1)
+    x_ticks = list(range(1, len(files), 6))
+    x_ticks.append(len(files))
+    
+    for call in mpi_calls:
+        
+    
+        plt.figure(figsize=(8, 8))
+        sns.set_theme(style="whitegrid", palette="pastel")
+        # Plot means
+        plt.subplot(3, 1, 1)
+        plt.plot(x, means[call], marker='o', label='Mean')
+        plt.grid(True)
+        plt.xticks(x_ticks)
+        plt.xlabel('Process Number')
+        plt.ylabel('Runtime (us)')
+        plt.title(f'{call} - Mean Runtime')
+        plt.legend()
+        
+        # Plot mins
+        plt.subplot(3, 1, 2)
+        plt.plot(x, mins[call], marker='v', label='Min')
+        plt.grid(True)
+        plt.xticks(x_ticks)
+        plt.xlabel('Process Number')
+        plt.ylabel('Runtime (us)')
+        plt.title(f'{call} - Min Runtime')
+        plt.legend()
+        
+        # Plot maxs
+        plt.subplot(3, 1, 3)
+        plt.plot(x, maxs[call], marker='^', label='Max')
+        plt.grid(True)
+        plt.xticks(x_ticks)
+        plt.xlabel('Process Number')
+        plt.ylabel('Runtime (us)')
+        plt.title(f'{call} - Max Runtime')
+        plt.legend()
+        
+        plt.tight_layout()
+        plt.savefig(f'plots/performance_metrics_{call}.png')
+        plt.close()
+
+#process_file_reprompi()
+process_file_cppmafia()
+#process_file_sort()
--- scripts/runner.sh
+++ scripts/runner.sh
@@ -4,20 +4,53 @@
 
 # Define the number of repetitions
 nrep=1000
+totalCPU=48
 
 # Iterate over the process numbers
-for num in {001..36};
-do
-    # Generate the output file name
-    output_file="bmark_np${num}_nrep${nrep}.log"
+function bmark_mpi() {
+    for num in {001..48};
+    do
+        # Generate the output file name
+        output_file="bmark_np${num}_nrep${nrep}.log"
+    
+        # Run the MPI benchmark
+        mpirun -np $num -hostfile mpi_hosts.conf ~/mpi_shared/ca-final-project/reprompi/bin/mpibenchmark \
+            --msizes-list=800 \
+            --nrep=$nrep \
+            --calls-list=MPI_Allgather,MPI_Allreduce,MPI_Barrier,MPI_Bcast,MPI_Reduce,MPI_Gather,MPI_Alltoall \
+            --summary=mean,min,max >| data/$output_file
+            #--output=data/$output_file
+        echo -e "[*] Generated ${output_file}"
+        sleep 1
+    done
+}
 
-    # Run the MPI benchmark
-    mpirun -np $num -hostfile mpi_hosts.conf ~/mpi_shared/ca-final-project/reprompi/bin/mpibenchmark \
-        --msizes-list=800 \
-        --nrep=$nrep \
-        --calls-list=MPI_Allgather,MPI_Allreduce,MPI_Barrier,MPI_Bcast,MPI_Reduce,MPI_Gather \
-        --summary=mean,min,max >| data/$output_file
-        #--output=data/$output_file
-    echo -e "[*] Generated ${output_file}"
-done
+function bmark_cppmafia() {
+    for num in {002..20};
+    do
+        # Generate the output file name
+        output_file="cppmafia_np${num}.log"
+        mpiexec -np 1 -hostfile mpi_hosts.conf \
+            ~/mpi_shared/ca-final-project/gpumafia/cppmafia/bin/cppmafia --timing ~/mpi_shared/ca-final-project/gpumafia/utils/clugen/cluster-test/cluster-$num.dat | grep total >| data/$output_file
+        echo -e "[*] Generated ${output_file}"
+        #sleep 1
+    done
+    
+}
 
+function bmark_sort() {
+    for num in {001..48};
+    do
+        # Generate the output file name
+        output_file="sort_np${num}.log"
+        mpiexec -np $num -hostfile mpi_hosts.conf \
+            ~/mpi_shared/ca-final-project/MP-sort/main 100000 | grep time >| data/$output_file
+        echo -e "[*] Generated ${output_file}"
+        sleep 2
+    done
+    
+}
+
+bmark_cppmafia
+#bmark_sort
+#bmark_mpi
